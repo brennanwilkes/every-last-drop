2ab023564d939c28c06d0239a42ec7f55a875a88
Brennan Wilkes <brennan@codexwilkes.com>
2020-10-20 13:21:15 -0700

    feat: script to print status of db

---
+++ src/sql/status.sql
@@ -0,0 +1,20 @@
+USE everyLastDrop;
+
+DESC glass;
+SELECT * FROM glass;
+DESC drinkRecipe;
+SELECT * FROM drinkRecipe;
+DESC drinkRequires;
+SELECT * FROM drinkRequires;
+DESC ingredient;
+SELECT * FROM ingredient;
+DESC alcohol;
+SELECT * FROM alcohol;
+DESC juice;
+SELECT * FROM juice;
+DESC garnish;
+SELECT * FROM garnish;
+DESC customer;
+SELECT * FROM customer;
+DESC transaction;
+SELECT * FROM transaction;
