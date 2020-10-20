f55579cc504b42cbb344ffe5418ab61747678bb3
Brennan Wilkes <brennan@codexwilkes.com>
2020-10-20 13:06:00 -0700

    feat: json to sql conversion script

---
+++ src/dev/jsonToSql.js
@@ -0,0 +1,59 @@
+const path = require("path")
+const fs = require('fs');
+
+const strWrap = val => typeof val === "string" ? `"${val}"` : val;
+
+const splitTupleJSON = json => {
+	let tuple = {col: "", data:""};
+	Object.keys(json).forEach((key, i) => {
+		tuple.col += (i ? ", " : "")+key;
+		tuple.data += (i ? ", " : "")+strWrap(json[key]);
+	});
+	return tuple;
+}
+
+
+if(process.argv.length < 4){
+	console.error("Invalid arguments");
+	process.exit(1);
+}
+
+const outputFile = process.argv[3];
+const json = require(path.join("..", "..", process.argv[2]));
+
+if(!json.database){
+	console.error("Invalid JSON has no database");
+	process.exit(1);
+}
+
+const orders = [];
+orders["database"] = json.database;
+
+let shift = 0;
+Object.keys(json).forEach((key, i) => {
+	if(key === "database"){
+		shift++
+	}
+	else{
+		orders.push(json[key]);
+		orders[i-shift]["table"] = key;
+	}
+});
+
+orders.sort((a,b) => a.order - b.order);
+
+let sql = `USE ${orders.database};\n\n`;
+let statements = 0;
+orders.forEach(order => {
+	order.data.forEach(tuple => {
+		tuple = splitTupleJSON(tuple);
+		sql += `INSERT INTO ${order.table}(${tuple.col})\n`
+		sql += `VALUES (${tuple.data}); \n\n`;
+		statements++;
+	});
+});
+
+fs.writeFile(outputFile, sql, function (err) {
+	if (err) throw err;
+	console.log(`wrote ${statements} SQL statements to ${outputFile}`);
+});
