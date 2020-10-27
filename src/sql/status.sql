d385e03e900f0490d8d7fdc506f2a915c95764ed
Brennan Wilkes <brennan@codexwilkes.com>
2020-10-26 20:55:02 -0700

    fix(status.sql): Added new tables

--- src/sql/status.sql
+++ src/sql/status.sql
@@ -2,19 +2,39 @@ USE everyLastDrop;
 
 DESC glass;
 SELECT * FROM glass;
+
 DESC drinkRecipe;
 SELECT * FROM drinkRecipe;
+
+DESC drinkRating;
+SELECT * FROM drinkRating;
+
 DESC drinkRequires;
 SELECT * FROM drinkRequires;
+
 DESC ingredient;
 SELECT * FROM ingredient;
+
+DESC ingredientAvailable;
+SELECT * FROM ingredientAvailable;
+
 DESC alcohol;
 SELECT * FROM alcohol;
+
+DESC alcoholType;
+SELECT * FROM alcoholType;
+
 DESC juice;
 SELECT * FROM juice;
+
+DESC juiceFruit;
+SELECT * FROM juiceFruit;
+
 DESC garnish;
 SELECT * FROM garnish;
+
 DESC customer;
 SELECT * FROM customer;
+
 DESC transaction;
 SELECT * FROM transaction;
