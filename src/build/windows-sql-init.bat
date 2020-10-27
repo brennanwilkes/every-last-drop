34b3a1a3e9d676c3e01c0cd164a1b75d83d770f3
Brennan Wilkes <brennan@codexwilkes.com>
2020-10-27 11:26:49 -0700

    fix: Updated mariadb check for windows

--- src/build/windows-sql-init.bat
+++ src/build/windows-sql-init.bat
@@ -1,10 +1,11 @@
 :: I am not used to batch programming, or windows stuff at all
 :: so if you guys know better ways to do this stuff, please help me out :p
 
-WHERE mariadb
+dir "\Program Files\MariaDB*"
 if %ERRORLEVEL% == 0 goto :run
+ECHO Please install MariaDB
 exit
 
 :run
-net start mariadb
-mariadb -u root -p < src\sql\init.sql
+net start MariaDB
+MariaDB -u root -p < src\sql\init.sql
