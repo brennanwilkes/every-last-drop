a789d5289e03f11e16cc16d0ff1804b07114bf16
Brennan Wilkes <brennan@codexwilkes.com>
2020-11-08 23:49:54 -0800

    feat(login): Settings button

--- src/frontend/Login.js
+++ src/frontend/Login.js
@@ -1,19 +1,13 @@
 import React from "react";
 
 import "./bootstrap-import.js";
-import "./login.css";
+import { ImCog } from "react-icons/im";
 
 import barImage from "../../assets/bar-stock.jpg";
+import "./login.css";
+
 
 class Login extends React.Component {
-
-	staff() {
-		alert("Staff login");
-	}
-
-	customer() {
-		alert("Customer login");
-	}
 
 	componentDidMount() {
 		$("main").css("backgroundImage",`linear-gradient(
@@ -24,18 +18,23 @@ class Login extends React.Component {
 
 	render() {
 		return <>
-		<form className="text-center">
-			<h1 className="h2 mb-4 font-weight-normal">Every Last Drop</h1>
-			<label>
-				<input id="name" className="form-control" placeholder="Full Name" required autoFocus />
-			</label>
-			<label>
-				<input type="date" id="dob" className="form-control" required />
-			</label>
 
-			<button className="btn btn-lg btn-primary btn-block mt-4" type="submit">Continue to Menu</button>
-			<p className="mt-5 mb-0 text-muted">CPSC 2221 | W01 | Group 12</p>
-		</form>
+			<form className="text-center">
+
+				<button id="settings-button" className="btn btn-lg"><ImCog size={32} /></button>
+				<h1 className="h1 mb-4 font-weight-normal">Every Last Drop</h1>
+
+				<label>
+					<input id="name" className="form-control" placeholder="Full Name" required autoFocus />
+				</label>
+
+				<label>
+					<input type="date" id="dob" className="form-control" required />
+				</label>
+
+				<button className="btn btn-lg btn-primary btn-block mt-4" type="submit">Continue to Menu</button>
+				<p className="mt-5 mb-0 text-muted">CPSC 2221 | W01 | Group 12</p>
+			</form>
 		</>;
 	}
 }
