5dd1b2294c9e759db9f0d69b60f294e71b12cd54
Brennan Wilkes <brennan@codexwilkes.com>
2020-11-10 13:52:17 -0800

    fix(nav): Margin and padding adjustments

--- src/frontend/nav/Nav.js
+++ src/frontend/nav/Nav.js
@@ -3,8 +3,7 @@ import "../bootstrap-import.js";
 
 class User extends React.Component{
 	render(){
-		return <button type="button" className="btn btn-outline-secondary mr-4">{this.props.name}</button>
-;
+		return <button type="button" className="btn btn-outline-secondary ml-3 mr-3">{(!this.props.name) || this.props.name.length < 1 ? "Invalid User": this.props.name}</button>;
 	}
 
 }
@@ -13,13 +12,13 @@ class Nav extends React.Component{
 
 	render(){
 		return <>
-			<nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-				<a className="navbar-brand ml-3" href=".">Every Last Drop</a>
-				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse">
+			<nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark pt-3 pb-3">
+				<a className="navbar-brand ml-3" style={{marginRight:"-0.5em"}} href=".">Every Last Drop</a>
+				<button className="navbar-toggler mr-3" type="button" data-toggle="collapse" data-target="#navbarCollapse">
 					<span className="navbar-toggler-icon"></span>
 				</button>
 				<div className="collapse navbar-collapse" id="navbarCollapse">
-					<ul className="navbar-nav mr-auto">
+					<ul className="mr-3 ml-3 navbar-nav mr-auto">
 						<li className="nav-item active">
 							<a className="nav-link" href=".">Home</a>
 						</li>
