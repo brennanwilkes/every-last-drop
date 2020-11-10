006750f43b8fc326c74e561d68b8dd6b957e9479
Brennan Wilkes <brennan@codexwilkes.com>
2020-11-09 18:33:14 -0800

    feat: App state changes

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,19 +1,37 @@
 import React from "react";
-import "../bootstrap-import.js";
+import "./bootstrap-import.js";
 
 import Login from "./login/Login.js";
 import Dashboard from "./dashboard/Dashboard.js";
+
+const AppMode = Object.freeze({
+	"LOGIN":1,
+	"DASHBOARD":2,
+	"MENU":3
+});
 
 
 class App extends React.Component {
 
 	constructor(props){
 		super(props);
+		this.state = {
+			md : AppMode.LOGIN
+		}
 	}
 
 	render() {
 		return <>
-			<Login />
+			{
+				(this.state.md === AppMode.LOGIN)
+				? <Login
+					toDash={ () => this.setState({md:AppMode.DASHBOARD}) }
+					toMenu={ () => this.setState({md:AppMode.MENU}) }
+				/>
+				: ((this.state.md === AppMode.DASHBOARD)
+				? <Dashboard />
+				: <h1>Menu page</h1> )
+			}
 		</>;
 	}
 }
--- src/frontend/index.js
+++ src/frontend/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
-import App from "./login/Login.js";
+import App from "./App.js";
 
 //Main render
 ReactDOM.render(<App />, document.getElementsByTagName("MAIN")[0]);
