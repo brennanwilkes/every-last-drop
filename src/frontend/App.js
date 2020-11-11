127bf280fb9d8d31a8f700202d4b1b7a6be9abe4
Brennan Wilkes <brennan@codexwilkes.com>
2020-11-10 21:00:18 -0800

    chore: Switched default load state

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			md : AppMode.MENU,
+			md : AppMode.LOGIN,
 			user: undefined,
 			dob: undefined
 		}
