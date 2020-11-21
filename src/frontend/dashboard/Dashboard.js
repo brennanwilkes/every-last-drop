f9f40776d930219d378f96383bb1dfe35bf0bb56
Brennan Wilkes <brennan@codexwilkes.com>
2020-11-20 22:27:06 -0800

    feat(dashboard): Basic skeleton

--- src/frontend/dashboard/Dashboard.js
+++ src/frontend/dashboard/Dashboard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../bootstrap-import.js";
+import "./dashboard.css";
+
 
 
 class Dashboard extends React.Component {
@@ -10,7 +12,26 @@ class Dashboard extends React.Component {
 
 	render() {
 		return <>
-			<h1>Dashboard</h1>
+			<div className="container-fluid p-3" id="dashboard">
+				<div className="row">
+					<div className="col-md-4 p-3">
+						1
+					</div>
+					<div className="col-md-8 p-3">
+						<div className="row p-3">
+							2
+						</div>
+						<div className="row p-3">
+							3
+						</div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-md-12 p-3">
+						4
+					</div>
+				</div>
+			</div>
 		</>;
 	}
 }
