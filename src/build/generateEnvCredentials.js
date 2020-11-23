2eb62ad1799c992956a0f8a33b468a1674924f7a
Brennan Wilkes <brennan@codexwilkes.com>
2020-11-23 13:29:54 -0800

    feat: Auto .env creation

---
+++ src/build/generateEnvCredentials.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const readline = require("readline");
+const fs = require('fs');
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+rl.question("Please enter a username to use for this application: ", function(user) {
+	rl.question("Please enter a password to use for this application: ", function(pass) {
+
+		const AUTH = `ADMIN_USER=${user}
+ADMIN_PASS=${pass}
+`;
+
+	console.log(AUTH)
+
+		fs.writeFile(path.join(__dirname,"..","..",".env"), AUTH, function (err) {
+			if (err){
+				throw err;
+				process.exit(1);
+			}
+			console.log(`Generated secure user credentials`);
+			process.exit(0);
+		});
+
+		rl.close();
+	});
+});
