2502c9131a92b0d6f0a4a1b8cca6b6237c2d36e5
Brennan Wilkes <brennan@codexwilkes.com>
2020-11-23 13:40:57 -0800

    fix(dev): Linux distro sudo/-u

---
+++ src/dev/linux-sql-upload.sh
@@ -0,0 +1,23 @@
+
+cat /etc/os-release | head -n1 | grep '[aA]mazon' >/dev/null
+if [ "$?" -ne 0 ]; then
+	#NON AMAZON LINUX
+	mariadb --version 2>/dev/null >/dev/null
+
+	if [ "$?" -eq 0 ]; then
+		sudo mariadb -u root -p < "$1"
+	else
+		echo "mariadb is not installed. Try:"
+		echo "'sudo apt install mariadb-server'";
+	fi;
+else
+	#AMAZON LINUX
+	sudo service mysql start >/dev/null 2>/dev/null
+	mariadb --version 2>/dev/null >/dev/null
+	if [ "$?" -eq 0 ]; then
+		mariadb -u root < "$1"
+	else
+		echo "mariadb is not installed. Try:"
+		echo "'sh ./src/build/amazon-install-mariadb.sh'";
+	fi;
+fi
