a5493b92e04738ba2a183e28d23ac6f7701a2183
Brennan Wilkes <brennan@codexwilkes.com>
2020-11-24 00:44:26 -0800

    docs: Build docs

--- src/build/generateEnvCredentials.js
+++ src/build/generateEnvCredentials.js
@@ -1,19 +1,28 @@
+//Brennan Wilkes
+
+//Includes
 const path = require("path");
 const readline = require("readline");
 const fs = require('fs');
 
+//Create readline interface, initialize to defaults
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
 
+//Prompt user for an everyLastDrop username
 rl.question("Please enter a username to use for this application: ", function(user) {
+
+	//Prompt user for an everyLastDrop password
 	rl.question("Please enter a password to use for this application: ", function(pass) {
 
+		//Build AUTH string for .env file
 		const AUTH = `ADMIN_USER=${user}
 ADMIN_PASS=${pass}
 `;
 
+		//Write to .env
 		fs.writeFile(path.join(__dirname,"..","..",".env"), AUTH, function (err) {
 			if (err){
 				throw err;
--- src/build/generateSqlUser.js
+++ src/build/generateSqlUser.js
@@ -1,16 +1,23 @@
+//Brennan Wilkes
+
+//Includes
 const path = require("path");
 const fs = require('fs');
 require('dotenv').config();
 
+//Get config. Needed for database name and host
 const {CONFIG} = require(path.join(__dirname,"..","backend","server.js"));
 
+//Grab username and password from enviornment variables
 const ADMIN_USER = process.env.ADMIN_USER;
 const ADMIN_PASS = process.env.ADMIN_PASS;
 
+//Build SQL permissions string
 const PERMISSIONS = `GRANT all privileges ON ${CONFIG["database"]}.* TO '${ADMIN_USER}'@'${CONFIG["host"]}' IDENTIFIED BY '${ADMIN_PASS}';
 FLUSH PRIVILEGES;
 `;
 
+//Write to special .sqlUserEnv file
 fs.writeFile(path.join(__dirname,"..","..","config",".sqlUserEnv"), PERMISSIONS, function (err) {
 	if (err) throw err;
 	console.log(`Generated secure SQL user string`);
