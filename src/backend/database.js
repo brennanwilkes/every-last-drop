a56f08b0c74816d7caa0238dd3c6d5797eefb44d
Brennan Wilkes <brennan@codexwilkes.com>
2020-11-26 13:27:55 -0800

    docs: Backend docs

--- src/backend/database.js
+++ src/backend/database.js
@@ -1,76 +1,186 @@
+//Brennan Wilkes
+
+//Includes
 const mariadb = require('mariadb');
 const path = require("path");
+var fs = require('fs');
+
+//Load secure ENV variables
 require('dotenv').config();
-var fs = require('fs')
 
 //Pull utility functions from server.js
 const {print,CONFIG} = require(path.join(__dirname,"server.js"));
 
 
-/*
-	Database connection object
+/**
+	Database connection abstraction.
+	Handles basic SQL get requests as well as auto connection handling
+	and insertion statement building.
+	Utilizes prepared statements for security.
+	@class
+	@memberof backend
 */
 exports.database = {
-	username: process.env.ADMIN_USER,
-	password: process.env.ADMIN_PASS,
-	database: CONFIG["database"],
-	hostname: CONFIG["host"],
-	connectionLimit: 5,
 
+	/**
+		MariaDB username with admin privileges.
+		Loaded from secure ENV variable
+		@type {string}
+	*/
+	username: process.env.ADMIN_USER,
+
+	/**
+		MariaDB password for account with admin privileges.
+		Loaded from secure ENV variable
+		@type {string}
+	*/
+	password: process.env.ADMIN_PASS,
+
+	/**
+		Database name to connect to.
+		Loaded from backend config file
+		@type {string}
+	*/
+	database: CONFIG["database"],
+
+	/**
+		hostname to connect to.
+		Loaded from backend config file
+		@type {string}
+	*/
+	hostname: CONFIG["host"],
+
+	/**
+		Max connections at one time.
+		Loaded from backend config file.
+		@type {number}
+	*/
+	connectionLimit: CONFIG["connectionLimit"],
+
+	/**
+		Initializes mariadb pool
+		Loads config options and sets up the DB socket.
+	*/
 	init(){
-		const params = {host: this.hostname, user: this.username, password: this.password, connectionLimit: this.connectionLimit, database: this.database};
+
+		//Create parameters for pool
+		const params = {
+			host: this.hostname,
+			user: this.username,
+			password: this.password,
+			connectionLimit: this.connectionLimit,
+			database: this.database
+		};
+
+		//Create mariadb pool
 		this.pool = mariadb.createPool(params);
-		print("Created MariaDB Pool",JSON.stringify(params, null, 4))
+
+		//Hide password in output
+		params.password = "*".repeat(params.password.length);
+
+		//Debug
+		print("Created MariaDB Pool with options",JSON.stringify(params, null, 4));
 	},
+
+	/**
+		Connects to the database. Blocks until complete instead of returning a promise.
+		Future revisions may switch to promise implementation.
+		@asnyc
+		@returns {object} Socket connection object to run queries on. Undefined behaviour on connection failure.
+	*/
 	async connect(){
+
+		//Server is already connected to
 		if(this.connection){
+
+			//Return cached connection
 			return this.connection
 		}
 		try{
+
+			//Create socket
 			this.connection = await this.pool.getConnection();
+
+			//Debug
 			print(`Successfully connected to MariaDB ${this.username}@${this.hostname}`);
 			return this.connection;
 		}
 		catch(error){
+
+			//Error reporting
 			print(`Failed to connect to MariaDB ${this.username}@${this.hostname}`);
 			print(error);
 		}
 	},
 
+	/**
+		Runs a SQL query via a prepared statement and resolves to the results
+		@param {string} query SQL query to run, with data as '?'
+		@param {any[]} data Array of data to insert
+		@returns {Promise} Promise which resolves to the results of the SQL query
+	*/
 	async get(query,data){
+
+		//Ensure connection is established
 		const conn = await this.connect();
 
+		//Debug
 		if(data){
-			print(`Querying ${query}, with data ${data.join(" ")} using ${this.database}`)
+			print(`Querying ${query}, with data ${data.join(" ")} using ${this.database}`);
 		}
 		else{
-			print(`Querying ${query} using ${this.database}`)
+			print(`Querying ${query} using ${this.database}`);
 		}
+
+		//Query
 		const res = await conn.query(query,data);
 		return res;
 	},
 
+	/**
+		Automatically builds an insertion query and runs it
+		@param {string} table SQL table to insert into
+		@param {object} tuple Tuple contains key value pairs for columns and data
+		@param {boolean} ignore Toggle for 'INSERT IGNORE' mode. Defaults to false
+		@returns {Promise} Promise which resolves to the result of the query
+	*/
 	async insert(table,tuple,ignore=false){
+
+		//Ensure connection is established
 		const conn = await this.connect();
 
+		//Split tuple into column headers and data lists
 		let header = "";
 		let values = "";
 		const data = [];
 		Object.keys(tuple).forEach(key => {
 			header += key + ", ";
 			values += "?, ";
-			data.push(tuple[key])
+			data.push(tuple[key]);
 		});
+
+		//Build SQL string
 		let sql = `INSERT ${ignore?"IGNORE ":""}INTO ${table}(${header.substring(0,header.length-2)})`;
 		sql += `VALUES (${values.substring(0,values.length-2)});`;
 
-		print(`Inserting ${JSON.stringify(tuple,null,4)} into ${this.database}`)
+		//Debug
+		print(`Inserting ${JSON.stringify(tuple,null,4)} into ${this.database}`);
+
+		//Query
 		const res = await conn.query(sql,data);
 		return res;
 	},
 
+	/**
+		Securely disconnections from the database
+		@returns {object} Disconnect data
+	*/
 	disconnect(){
-		print("Disconnected from MariaDB")
+
+		//Debug
+		print("Disconnected from MariaDB");
+
+		//Disconnet
 		if (this.connection) return this.connection.end();
 	},
 };
--- src/backend/server.js
+++ src/backend/server.js
@@ -1,42 +1,92 @@
+/**
+	@namespace backend
+	@since 23/10/2020
+	@version 1.0
+	@author Brennan Wilkes
+*/
+
+
+//Includes
 const express = require('express');
 const path = require("path");
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 
-//Store all backend config vars here
+//Load backend config vars here
 const CONFIG = require(path.join(__dirname,"..", "..","config","backend.json"));
 exports.CONFIG = CONFIG
 
-//Just a config switch for printing
+/**
+	Prints to stdout if verbose config mode is set
+	@param {string[]} content Content strings to print
+	@memberof backend
+*/
 const print = (...content) => {
 	if(CONFIG.verbose) console.log(...content);
 };
 exports.print = print
 
 
-/*
-	Server backend object
+/**
+	Server abstration object
+	@class
+	@memberof backend
 */
 exports.server = {
-	port : process.env.PORT || CONFIG.port,
+
+	/**
+		Port to use. Defaults to a "PORT" env variable if set (For heroku and other deployment methods),
+		otherwise uses the port set in the config file. Defaults to 8080.
+		@type {number}
+	*/
+	port : process.env.PORT || CONFIG.port || 8080,
+
+	/**
+		Express init object
+		@type {object}
+	*/
 	app: express(),
 
+	/**
+		Creates a promise based express route.
+		This abstraction makes repetitive routing more robust.
+		Handles errors by default.
+		@param {string} path URI to listen for
+		@param {function} function A function which should return a promise to evaluate. The resolution value will be sent to the client with 200. Errors will be sent with 409.
+		@param {string} method Routing method. Defaults to GET.
+	*/
 	route(path,promise,method="get"){
+
+		//Cleanup path URI
 		path = path.substring(0,1) === "/" ? path : "/"+path;
 
+		//Set up route
 		this.app[method](path, (req,res) => {
+
+			//Run promise method
 			promise(req).then(data => {
+
+				//Send results to client
 				res.send(data);
 				res.end();
 			}).catch(error => {
+
+				//Error handling
 				res.status(409);
 				res.send("<h1>Internal Error</h1><br>"+error);
 				res.end();
 			});
 		});
-		print(`Setup ${method} route for ${path}`)
+
+		//Debug
+		print(`Setup ${method} route for ${path}`);
 	},
 
+	/**
+		Basic express setup.
+		Sets JSON encoding, url encoded bodies and static routing.
+		Should be run prior to any routing.
+	*/
 	init(){
 
 		// support json encoded bodies
@@ -49,19 +99,28 @@ exports.server = {
 		this.app.use('/', express.static(path.join(__dirname,"..", "..", "public")));
 	},
 
+	/**
+		Starts the webserver.
+		This method should be run last, after init and routing.
+	*/
 	start(){
 
-		//404 messages
+		//Create 404 messages for all uncaught routes
 		this.app.get('*', (req, res) =>{
+
+			//Debug
 			print("Received invalid GET request for",req.url);
 
+			//Response
 			res.writeHead(404, {'Content-Type': 'text/html'});
 			res.write("<h1>404: If you see this, it's already too late!</h1>");
 			res.end();
 		});
 
-		//Socket init
+		//Initialize the server socket and start the webserver
 		this.server = this.app.listen(this.port, () => {
+
+			//Debug
 			print('server is listening on port', this.server.address().port);
 		});
 	}
