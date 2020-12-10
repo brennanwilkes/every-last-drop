379bc8dfa27b3ce6c4449dbc106d5a48337c59c2
Brennan Wilkes <brennan@codexwilkes.com>
2020-12-10 13:18:58 -0800

    feat: Removed default env override

--- src/build/generateEnvCredentials.js
+++ src/build/generateEnvCredentials.js
@@ -4,12 +4,21 @@
 const path = require("path");
 const readline = require("readline");
 const fs = require('fs');
+require('dotenv').config();
 
 //Create readline interface, initialize to defaults
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
+
+const ADMIN_USER = process.env.ADMIN_USER;
+const ADMIN_PASS = process.env.ADMIN_PASS;
+
+if(ADMIN_PASS && ADMIN_USER){
+	console.log(`Env credentials already exist!`);
+	process.exit(0);
+}
 
 //Prompt user for an everyLastDrop username
 rl.question("Please enter a username to use for this application: ", function(user) {
