c1cf66653afe4885e4af33e1eeb1d5ac9564457b
Brennan Wilkes <brennan@codexwilkes.com>
2020-12-11 18:15:02 -0800

    ci: Switched to sql-deployment script

--- src/build/deployment-sql.sh
+++ src/build/deployment-sql.sh
@@ -2,7 +2,5 @@
 #sudo mariadb -u root < config/.sqlUserEnv;
 #sudo mariadb -u root < "$1"
 
-#ssh -p 2222 brennan@pi.codexwilkes.com 'cd LMPServer ; screen -r'
-
 echo "DEPLOYMENT SQL"
-echo "$SQL_SSH_STRING"
+eval "$SQL_SSH_STRING" 'myArg'
