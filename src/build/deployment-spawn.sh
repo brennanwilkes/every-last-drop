79330337a390287ebaca740d7860a5b32d5d226e
Brennan Wilkes <brennan@codexwilkes.com>
2020-12-11 23:36:55 -0800

    ci: Proper quoting to eval

--- src/build/deployment-spawn.sh
+++ src/build/deployment-spawn.sh
@@ -1,1 +1,1 @@
-eval "$SQL_SSH_STRING" "curl -s https://raw.githubusercontent.com/brennanwilkes/every-last-drop/master/src/build/deployment-sql.sh | sh -s \"$ADMIN_USER\" \"$ADMIN_PASS\""
+eval "$SQL_SSH_STRING" "\"curl -s https://raw.githubusercontent.com/brennanwilkes/every-last-drop/master/src/build/deployment-sql.sh | sh -s \\\"$ADMIN_USER\\\" \\\"$ADMIN_PASS\\\"\""
