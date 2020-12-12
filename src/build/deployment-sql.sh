8d2ef2ee2c8eaf58062efede319215667ecf148b
Brennan Wilkes <brennan@codexwilkes.com>
2020-12-11 23:47:24 -0800

    ci: Switched to here doc extras file auth

--- src/build/deployment-sql.sh
+++ src/build/deployment-sql.sh
@@ -3,14 +3,22 @@ user="$1"
 pass="$2"
 
 TEMP=$( mktemp )
+credentialsFile=$( mktemp )
+
+cat << EOF > "$credentialsFile"
+[client]
+user=$user
+password=$pass
+EOF
 
 curl -s "https://raw.githubusercontent.com/brennanwilkes/every-last-drop/master/src/sql/init.sql" > "$TEMP"
-mariadb -u "$user" -p "$pass" < "$TEMP"
+mariadb --defaults-extra-file="$credentialsFile" < "$TEMP"
 
 curl -s "https://raw.githubusercontent.com/brennanwilkes/every-last-drop/master/data/sample-data.sql" > "$TEMP"
-mariadb -u "$user" -p "$pass" < "$TEMP"
+mariadb --defaults-extra-file="$credentialsFile" < "$TEMP"
 
 curl -s "https://raw.githubusercontent.com/brennanwilkes/every-last-drop/master/data/transaction-data.sql" > "$TEMP"
-mariadb -u "$user" -p "$pass" < "$TEMP"
+mariadb --defaults-extra-file="$credentialsFile" < "$TEMP"
 
 rm "$TEMP"
+rm "$credentialsFile"
