a4d45fec227213b4ee4d2499266e8eb6cf89a17f
Brennan Wilkes <brennan@codexwilkes.com>
2020-12-13 12:04:55 -0800

    ci: Config port debug

--- src/build/deployment-config.js
+++ src/build/deployment-config.js
@@ -5,11 +5,14 @@ const configName = path.join(__dirname,"..", "..","config","backend.json");
 
 var config = require(configName);
 
-config.verbose = false;
+//config.verbose = false;
 config.host = process.env.SQL_HOSTNAME || "localhost";
 if(process.env.SQL_PORT){
 	config.sqlPort = process.env.SQL_PORT;
 }
+
+console.log(config.sqlPort);
+console.log(config.host);
 
 fs.writeFile(configName, JSON.stringify(config,null,4), err => {
 	if(err) console.error(err)
